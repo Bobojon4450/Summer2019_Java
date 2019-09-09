22f074f70889f9e0f018472c1690cb7d8f9a477d
Bobojon4450 <bobojon.rva@gmail.com>
2019-09-09 14:16:25 -0400

    Muhtar's regex

---
+++ src/day17_StringClass/WarmUp.java
@@ -0,0 +1,8 @@
+package day17_StringClass;
+
+public class WarmUp {
+	public static void main(String[] args) {
+		String name = "Cybertek"+ 1;//we can concat anything with + sign and string
+//		String name2 = "Cybertek".concat(12);//impossible to pass number to concat(), cause it accepts string only.
+	}
+}

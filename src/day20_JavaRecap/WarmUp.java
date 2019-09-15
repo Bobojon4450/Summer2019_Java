35f7273a3f4e15165fe1e11a2d38e276fca15336
Bobojon4450 <bobojon.rva@gmail.com>
2019-09-15 15:45:25 -0400

    day20 actitvities

---
+++ src/day20_JavaRecap/WarmUp.java
@@ -0,0 +1,38 @@
+package day20_JavaRecap;
+
+import java.util.Scanner;
+
+public class WarmUp {
+
+	public static void main(String[] args) {
+		 /*
+	     	4. write a program that asks user to enter number 5 times:
+	        1. find the maximum number 
+	     */
+//		Scanner scan= new Scanner(System.in);
+//		
+//        int max = -99999999 ; //max = 100 , max =200
+//        
+//        for(int i = 1; i <= 5; i++ ) {
+//            System.out.println("Enter a number");
+//            int inputNum = scan.nextInt(); //1 , -2
+//                if(inputNum > max) {
+//                    max = inputNum;
+//                }
+//        }
+//        System.out.println("max value is : "+max);     
+        
+        
+		for (int i = 65; i <= 90; i++) {
+			System.out.print((char) i + "");
+		}
+		System.out.println();
+
+		for (int i = 97; i <= 122; i++) {
+			System.out.print((char) i + "");
+		}
+        
+        
+        
+	}
+}
---
+++ src/day20_JavaRecap/WarmUp5.java
@@ -0,0 +1,19 @@
+package day20_JavaRecap;
+
+public class WarmUp5 {
+
+	public static void main(String[] args) {
+		/*
+		 * print all upper and lower case letters of ascii table
+		 */
+		for (char ch = 'a'; ch <= 'z'; ch++) {
+			System.out.print(ch+" ");
+		}
+		System.out.println();
+		
+		for (char ch = 'A'; ch <= 'Z'; ch++) {
+			System.out.print(ch+" ");
+		}
+	}
+
+}
